0c4f4c64248d1c3ab10c1b0658c414b9f06c9d65
Christian Bell <camaribell@yahoo.com>
2023-05-18 14:48:50 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import React from 'react';
-import './App.css';
-
-import UserInterface from "./UserInterface"
+import React from "react";
+import "./App.css";
+import StyleContext from "./StyleContext";
+import UserInterface from "./UserInterface";
 
 function App() {
+  const styling = {
+    background: "#ff3300",
+    display: "inline-block",
+    width: 120,
+    height: 55,
+    fontSize: 18,
+    letterSpacing: 1,
+    border: "2px solid #ff0000",
+    borderRadius: 15,
+    color: 'white',
+    frontWeight:600
+  };
+
   return (
     <main>
-     <h1
+      <StyleContext.Provider value={ styling }>
+        <h1
           style={{
-            color: "#FEE001",
+            color: "#FF",
             fontFamily: "Luminari, fantasy",
-            fontSize: 50
+            fontSize: 50,
           }}
         >
           Welcome to Comics Galore!
         </h1>
         <UserInterface />
+      </StyleContext.Provider>
+      <h1
+        style={{
+          color: "#FEE001",
+          fontFamily: "Luminari, fantasy",
+          fontSize: 50,
+        }}
+      >
+        Welcome to Comics Galore!
+      </h1>
+      <UserInterface />
     </main>
   );
 }
---
+++ src/StyleContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// Create a new Context object
+const StyleContext = React.createContext({});
+
+export default StyleContext;
+
